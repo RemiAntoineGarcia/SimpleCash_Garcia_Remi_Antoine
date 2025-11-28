e303f92c49f84cb89cba2fc9dbaf4ef9d1d112d0
RemiAntoineGarcia <79006460+RemiAntoineGarcia@users.noreply.github.com>
2025-11-28 09:45:54 +0100

    init: ajout projet

---
+++ src/main/java/com/formation/simplecash_garcia_remi_antoine/SimpleCashGarciaRemiAntoineApplication.java
@@ -0,0 +1,13 @@
+package com.formation.simplecash_garcia_remi_antoine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCashGarciaRemiAntoineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleCashGarciaRemiAntoineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/formation/simplecash_garcia_remi_antoine/SimpleCashGarciaRemiAntoineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formation.simplecash_garcia_remi_antoine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleCashGarciaRemiAntoineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
